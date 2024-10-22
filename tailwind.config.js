6611d463d30e32561ce034156f1d6e359cc1ec05
thiramet27 <thirametphuthong@gmail.com>
2024-10-22 15:34:11 +0700

    CREATE:JS PROJECT

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}", "./public/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
